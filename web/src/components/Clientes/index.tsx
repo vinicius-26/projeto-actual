6480250802d84eaa1170f56024c2336f773096df
Vinicius <vistephano123@gmail.com>
2021-11-23 13:43:10 -0300

    Alterações da pagina de clientes

--- web/src/components/Clientes/index.tsx
+++ web/src/components/Clientes/index.tsx
@@ -9,37 +9,54 @@ const Clientes: React.FC = () => {
         <div className={styles.gradient}>  </div>
 
         <div className={styles.gridImages}>
-          <div className={styles.cliente1}>
-            <img src="img/logo.villa.jpg" alt="" />
-          </div>
+            <div className={styles.clientes2}>
+              <img src="img/clientes/cafeRaphaelli.png" alt="" />
+            </div>
+            <a href="https://www.carmomaq.com.br/" target="_blank"> <img src="img/clientes/logo-carmomaq-01.png" alt="" /></a>
 
-          <div className={styles.cliente2}>
-            <img src="img/cafeRaphaelli.png" alt="" />
-          </div>
+            <a href="https://www.nestle.com.br/" target="_blank"> <img src="img/clientes/nestle.png" alt="" /></a>
 
-          <div className={styles.cliente3}>
-            <a href="https://www.carmomaq.com.br/" target="_blank"> <img src="img/logo-carmomaq-01.png" alt="" /></a>
-          </div>
+            <a href="https://cafetialuiza.com.br/" target="_blank"> <img src="img/clientes/logo_cafe_tia_luiza.png" alt="" /></a>
 
-          <div className={styles.cliente4}>
-            <a href="http://www.montealegre.com.br/pt/" target="_blank"><img src="img/monte-alegre.png" alt="" /></a>
-          </div>
+            <a href="https://www.olamgroup.com/" target="_blank"><img src="img/clientes/olam.png" alt="" /></a>
 
-          <div className={styles.cliente5}>
-            <a href="http://www.terraadorada.com.br/" target='_blank'> <img src="img/terradorada.png" alt="" /></a>
-          </div>
+            <a href="https://coopfam.com.br/" target="_blank"> <img src="img/clientes/coopfam.png" alt="" /></a>
 
-          <div className={styles.cliente6}>
-            <a href="https://cafesoberano.com.br/" target="_blank"><img src="img/cafe-soberano.png" alt="" /></a>
-          </div>
+            <a href="https://astecahinomoto.com.br/pb/" target="_blank"><img src="img/clientes/logo_asteca.png" alt="" /></a>
 
-          <div className={styles.cliente7}>
-            <a href="https://www.aumacafe.com.br/" target="_blank"><img src="img/auma-cafe.jpg" alt="" /></a>
-          </div>
+            <a href="https://bourboncoffees.com.br/" target="_blank"> <img src="img/clientes/logo-header_bournon.png" alt="" /></a>
 
-          <div className={styles.cliente8}>
-            <a href="http://cafeterradagente.com.br/" target="_blank"><img src="img/terra-da-gente.jpg" alt="" /></a>
-          </div>
+            <a href="https://hatsuiacoffee.com.br/" target="_blank"> <img src="img/clientes/hatsuia.jpg" alt="" /></a>
+
+            <a href="https://stocklerltda.com.br/" target="_blank"> <img src="img/clientes/nkg-stockler.svg" alt="" /></a>
+
+            <a href="https://www.cooxupe.com.br/" target="_blank"><img src="img/clientes/cooxupe.png" alt="" /></a>
+
+            <a href="https://www.carmocoffees.com.br/?lang=pt-br" target="_blank"><img src="img/clientes/carmocoffees.png" alt="" /></a>
+
+            <a href="https://www.cafeesporte.com.br/" target="_blank"><img src="img/clientes/cafe-esporte.png" alt="" /></a>
+
+            <a href="https://www.cafe35.com.br/" target="_blank"><img src="img/clientes/cafe-35.png" alt="" /></a>
+
+            <a href="http://www.montealegre.com.br/pt/" target="_blank"><img src="img/clientes/monte-alegre.png" alt="" /></a>
+
+            <a href="https://goo.gl/maps/fVF3xofLMkjJfm8k8" target="_blank"><img src="img/clientes/logo.villa.jpg" alt="" /></a>
+
+            <a href="http://www.terraadorada.com.br/" target='_blank'> <img src="img/clientes/terradorada.png" alt="" /></a>
+
+            <a href="https://cafesoberano.com.br/" target="_blank"><img src="img/clientes/cafe-soberano.png" alt="" /></a>
+
+            <a href="https://www.aumacafe.com.br/" target="_blank"><img src="img/clientes/auma-cafe.jpg" alt="" /></a>
+
+            <a href="https://www.tabajara.ind.br/cafetabajara" target="_blank"><img src="img/clientes/LOGO-TABAJARA-01.png" alt="" /></a>
+
+            <a href="http://cafeterradagente.com.br/" target="_blank"><img src="img/clientes/cafe-terra-da-gente.jpg" alt="" /></a>
+
+            <a href="https://www.lukealimentos.com.br/luke/" target="_blank"><img src="img/clientes/luke.png" alt="" /></a>
+
+            <a href="http://sanjeralimentos.com.br/" target="_blank"><img src="img/clientes/sanjer.svg" alt="" /></a>
+
+            <a href="https://sucafina.com/" target="_blank"><img src="img/clientes/sucafina-logo-vector.png" alt="" /></a>
         </div>
         <div className={styles.gradientInvert}> </div>
       </div>
